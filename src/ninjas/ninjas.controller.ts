7967d961f63667e0b3253dca00ee59f8ca5fd0be
Rehan41302 <saeedrehan3@gmail.com>
2023-04-28 21:55:22 +0500

    feat: added services ninjas crud routes

--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -1,37 +1,45 @@
 import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
 import { CreateNinjaDto } from './dto/create-ninja.dto';
+import { UpdateNinjaDto } from './dto/update-ninja.dto';
+import { NinjasService } from './ninjas.service';
 
 @Controller('ninjas')
 export class NinjasController {
-    // GET /ninjas --> []
+    // Traditional way: const ninjasService = new NinjasService()
+    // NestJs way to create instance of the service class.
+    // It works because of injectables directive in the service class.
+    constructor(private readonly ninjasService: NinjasService) { }
+
+    // GET /ninjas?weapon=some weapon --> []
     @Get()
-    getNinjas(@Query('type') type: string) {
-        return [{ type }];
+    getNinjas(@Query('weapon') weapon: 'starts' | 'nunchucks') {
+        return this.ninjasService.getNinjas(weapon)
     }
 
     // GET /ninjas/:id --> {...}
+    // `:id` is extracted from the route params and passed as the variable `id`
     @Get(':id')
     getOneNinja(@Param('id') id: string) {
-        return {};
+        return this.ninjasService.getOneNinja(id)
     }
 
     // POST /ninjas
+    // Body extracted from request and matched with `CreateNinjaDto` type
     @Post()
     createNinja(@Body() createNinjaDto: CreateNinjaDto) {
-        return {
-            name: createNinjaDto.name
-        };
+        return this.ninjasService.createNinja(createNinjaDto)
     }
 
     // PUT /ninjas/:id --> {...}
+    // Body extracted from request and matched with `UpdateNinjaDto` type
     @Put(':id')
-    updateNinja(@Param('id') id: string) {
-        return {};
+    updateNinja(@Param('id') id: string, @Body() updateNinjaDto: UpdateNinjaDto) {
+        return this.ninjasService.updateNinja(id, updateNinjaDto)
     }
 
     // DELETE /ninjas/:id
     @Delete(':id')
     removeNinja(@Param('id') id: string) {
-        return {};
+        return this.ninjasService.removeNinja(id)
     }
 }
--- src/ninjas/ninjas.service.ts
+++ src/ninjas/ninjas.service.ts
@@ -1,4 +1,40 @@
 import { Injectable } from '@nestjs/common';
+import { CreateNinjaDto } from './dto/create-ninja.dto';
+import { UpdateNinjaDto } from './dto/update-ninja.dto';
 
 @Injectable()
-export class NinjasService {}
+export class NinjasService {
+    private ninjas = [
+        { id: 0, name: 'Ninja A', weapon: 'stars' },
+        { id: 1, name: 'Ninja B', weapon: 'nunchucks' }
+    ]
+
+    getNinjas(weapon?: 'starts' | 'nunchucks') {
+        if (weapon) {
+            return this.ninjas.filter(ninja => ninja.weapon === weapon)
+        }
+
+        return this.ninjas
+    }
+
+    getOneNinja(id: string) {
+        return this.ninjas.find(ninja => ninja.id === +id);
+    }
+
+    createNinja(createNinjaDto: CreateNinjaDto) {
+        const newNinja = { id: this.ninjas.length + 1, ...createNinjaDto }
+        this.ninjas.push(newNinja)
+        return newNinja
+    }
+
+    updateNinja(id: string, updateNinjaDto: UpdateNinjaDto) {
+        let ninjaIdx = this.ninjas.findIndex(n => n.id === +id)
+        this.ninjas[ninjaIdx] = { ...this.ninjas[ninjaIdx], ...updateNinjaDto }
+        return this.ninjas[ninjaIdx]
+    }
+
+    removeNinja(id: string) {
+        this.ninjas = this.ninjas.filter(n => n.id !== +id)
+        return {};
+    }
+}
